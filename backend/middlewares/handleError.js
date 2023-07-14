ed176b5eddc92eeaee0fe9c55b69fb5ec7f7956d
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-15 01:09:36 +0300

    fix13

--- backend/middlewares/handleError.js
+++ backend/middlewares/handleError.js
@@ -1,5 +1,6 @@
 const handleError = (err, req, res, next) => {
   console.log('fdffff');
+  console.log(err.message);
   const statusCode = err.statusCode || 500;
 
   const message = statusCode === 500 ? 'An error occurred on the server' : err.message;
