7d1d32a428ffba17664249a5b856ba95fea32621
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-15 14:21:48 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,13 @@ const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const handleError = require('./middlewares/handleError');
 const router = require('./routes/index');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-// app.use(cors({ origin: ['http://localhost:3000', 'https://mestofrontrex.nomoredomains.work'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://mestofrontrex.nomoredomains.work'] }));
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
